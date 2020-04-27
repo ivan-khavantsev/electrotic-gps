7be5eb4c30677f470b4d7ece287944129fffb4b2
Ivan Khavantsev <ivan.khavantsev@gmail.com>
2020-04-27 22:29:00 +0600

    Init commit

---
+++ atmel/project/main.c
@@ -0,0 +1,18 @@
+/*
+ * project.c
+ *
+ * Created: 27.04.2020 22:26:32
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
